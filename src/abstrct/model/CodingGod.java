962f928553e9d2f0a2b490533f1aaa5486fcbc8c
Joseph Al-Abudi <jala9132@740s-pl-120.local>
2017-04-10 13:42:22 -0600

    Added actions to the get methods

--- src/abstrct/model/CodingGod.java
+++ src/abstrct/model/CodingGod.java
@@ -17,35 +17,64 @@ public abstract class CodingGod implements Cody
 	
 	public String getKnowledge()
 	{
+		if(knowledge.contains("Genius"))
+		{
+			System.out.println("K n o w l e d g e");
+		}
+		else
+		{
+			System.out.println("You can't compare to the Coding God Himself");
+		}
 		return knowledge;
 	}
 	
 	public String getCode()
 	{
+		if(code.contains("Logic"))
+		{
+			System.out.println("You're taking the AP test, right?");
+		}
+		else
+		{
+			System.out.println("Shameful");
+		}
 		return code;
 	}
 	
 	public int getSpeed()
 	{
+		if(speed >= 20)
+		{
+			System.out.println("Speaking so fast leaving my ears in scorching flames of Coding Heat");
+		}
+		else
+		{
+			
+			System.out.println("You can't match his speed, nor can you match his momentum");
+		}
 		return speed;
 	}
 	
 	public boolean getCookingStatus()
 	{
-		return false;
+		if(isCooking = true);
+		{
+			System.out.println("He consumes sriracha and spits flames of Knowledge");
+		}
+		return isCooking;
 	}
 	
-	public void setKnowledge()
+	public void setKnowledge(String knowledge)
 	{
 		this.knowledge = knowledge;
 	}
 	
-	public void setCode()
+	public void setCode(String code)
 	{
 		this.code = code;
 	}
 	
-	public void setSpeed()
+	public void setSpeed(int speed)
 	{
 		this.speed = speed;
 	}
